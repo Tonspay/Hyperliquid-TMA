60bbc975a104578bc95afc48ac5e3656782bbaf7
Your Name <you@example.com>
2025-06-16 17:07:53 +0800

    add cloudstorage solutions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
 "use client";
 import { redirect } from 'next/navigation';
 import { useEffect } from 'react';
-
+import { cloudStorage } from "@telegram-apps/sdk-react";
+import { genWallet, resotreWallet } from 'core/web3';
+import { setKey } from 'core/storage';
 
 export default function Home({}) {
+  const keygen = async ()=>
+  {
+    //Check TMA cloudstorage status
+    try{
+      const path = `hyperliquid_key`;
+      const value =  resotreWallet(await cloudStorage.getItem(path));
+      console.log(value);
+      if(value)
+      {
+        //set key to localsotrage
+        setKey(value as any);
+        return redirect('/home');
+      }else{
+        //set new key to cloudstorage .
+        const sec = genWallet().privateKey;
+        await cloudStorage.setItem(path,sec)
+        window.location.reload();
+      }
+    }catch(e)
+    {
+      console.log(e)
+      return false;
+    }
+    return false;
+  }
+
   useEffect(() => {
-    redirect('/home');
+    keygen()
+    // redirect('/home');
     }, []);
 }
--- src/core/web3.ts
+++ src/core/web3.ts
@@ -3,22 +3,33 @@ import { getKey } from "./storage";
 
 const getWallet = () =>
 {
-    // const key = getKey();
-    const key = "";
+    const key = getKey();
+    // const key = "";
     if(!key)
     {
         return false;
     }else{
+        return resotreWallet(key)
+    }
+}
+
+const genWallet =() =>
+{
+    return ethers.Wallet.createRandom();
+}
+
+const resotreWallet = (key:string)=>
+{
         try{
             return new ethers.Wallet(key);
         }catch(e)
         {
             return false;
-        }
-       
-    }
+        }  
 }
 
 export {
-    getWallet
+    getWallet,
+    genWallet,
+    resotreWallet
 }
